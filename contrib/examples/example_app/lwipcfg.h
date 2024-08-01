4aa31d6f9912f684e37be9d73f572b704f461d5d
Goosmos <goosmos@ratio.myguest.virtualbox.org>
2024-08-01 11:35:24 +0900

    git ci del

--- contrib/examples/example_app/lwipcfg.h
+++ contrib/examples/example_app/lwipcfg.h
@@ -22,11 +22,11 @@
 /* #define USE_PCAPIF 1 */
 #define LWIP_PORT_INIT_IPADDR(addr)   IP4_ADDR((addr), 192,168,1,101)
 #define LWIP_PORT_INIT_GW(addr)       IP4_ADDR((addr), 127,0,0,1)
-#define LWIP_PORT_INIT_NETMASK(addr)  IP4_ADDR((addr), 255,0,0,0)
+#define LWIP_PORT_INIT_NETMASK(addr)  IP4_ADDR((addr), 0,0,0,0)
 
 /* remember to change this MAC address to suit your needs!
    the last octet will be increased by netif->num for each netif */
-#define LWIP_MAC_ADDR_BASE            {0x00,0x01,0x02,0x03,0x04,0x06}
+#define LWIP_MAC_ADDR_BASE            {0x00,0x01,0x02,0x03,0x04,0x05}
 
 /* #define USE_SLIPIF 0 */
 /* #define SIO_USE_COMPORT 0 */
@@ -55,14 +55,14 @@
 #define LWIP_NETIO_APP                0
 #define LWIP_MDNS_APP                 0
 #define LWIP_MQTT_APP                 0
-#define LWIP_PING_APP                 1
+#define LWIP_PING_APP                 0
 #define LWIP_RTP_APP                  0
 #define LWIP_SHELL_APP                0
 #define LWIP_SNMP_APP                 0
 #define LWIP_SNTP_APP                 0
 #define LWIP_SOCKET_EXAMPLES_APP      0
 #define LWIP_TCPECHO_APP              0
-#define LWIP_CLIENT_APP				  0
+#define LWIP_CLIENT_APP				  1
 /* Set this to 1 to use the netconn tcpecho server,
  * otherwise the raw api server will be used. */
 /*#define LWIP_TCPECHO_APP_NETCONN   */
